4910b2a8f3e99c4557486cfdcde70b8175a75f72
Manu Phatak <bionikspoon@gmail.com>
2015-12-21 17:12:38 -0600

    replace stubs

--- ch05/Prettify.hs
+++ ch05/Prettify.hs
@@ -1,11 +1,62 @@
 module Prettify (
-    Doc, char, double, fsep, hcat, punctuate, text, (<>)
+    Doc, char, double, empty, fsep, hcat, line, punctuate, text, (<>)
 ) where
 
-import           PrettyStub
+data Doc    = Empty
+            | Char Char
+            | Text String
+            | Line
+            | Concat Doc Doc
+            | Union Doc Doc
+              deriving (Show, Eq)
+
+empty :: Doc
+empty = Empty
+
+char :: Char -> Doc
+char = Char
+
+text :: String -> Doc
+text "" = Empty
+text s  = Text s
+
+double :: Double -> Doc
+double d = text (show d)
+
+line :: Doc
+line = Line
+
+(<>) :: Doc -> Doc -> Doc
+Empty <> y  = y
+x <> Empty  = x
+x <> y      = x `Concat` y
+
+hcat :: [Doc] -> Doc
+hcat = fold (<>)
+
+fsep :: [Doc] -> Doc
+fsep = fold (</>)
 
 
 punctuate :: Doc -> [Doc] -> [Doc]
 punctuate _ []      = []
 punctuate _ [d]     = [d]
 punctuate p (d:ds)  = (d <> p) : punctuate p ds
+
+flatten :: Doc -> Doc
+flatten (x `Concat` y)  = flatten x `Concat` flatten y
+flatten Line            = Char ' '
+flatten (x `Union` _)   = flatten x
+flatten other           = other
+
+fold :: (Doc -> Doc -> Doc) -> [Doc] -> Doc
+fold f = foldr f empty
+
+group :: Doc -> Doc
+group x = flatten x `Union` x
+
+softline :: Doc
+softline = group line
+
+(</>) :: Doc -> Doc -> Doc
+x </> y = x <> softline <> y
