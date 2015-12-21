bcbce3426f292f73bd0e978628a82e9d767c74a3
Manu Phatak <bionikspoon@gmail.com>
2015-12-21 15:59:32 -0600

    Pretty print JSON

---
+++ ch05/PrettyStub.hs
@@ -0,0 +1,28 @@
+module PrettyStub where
+
+data Doc = ToBeDefined
+           deriving (Show)
+
+-- string :: String -> Doc
+-- string _ = undefined
+
+text :: String -> Doc
+text _ = undefined
+
+double :: Double -> Doc
+double _ = undefined
+
+(<>) :: Doc -> Doc -> Doc
+_ <> _ = undefined
+
+char :: Char -> Doc
+char _ = undefined
+
+hcat :: [Doc] -> Doc
+hcat _ = undefined
+
+-- oneChar :: Char -> Doc
+-- oneChar _ = undefined
+
+-- hexEscape :: t -> Doc
+-- hexEscape _ = undefined
